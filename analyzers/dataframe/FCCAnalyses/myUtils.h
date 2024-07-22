96e190dd0b3ec56636180e12a6036fb5772a6ef3
axgallen <axgallen@lxplus.cern.ch>
2024-07-22 11:14:28 +0200

    Addition of selMC_leg structure

--- analyzers/dataframe/FCCAnalyses/myUtils.h
+++ analyzers/dataframe/FCCAnalyses/myUtils.h
@@ -77,6 +77,14 @@ namespace myUtils{
     ROOT::VecOps::RVec<int> operator() (ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> recop);
   };
 
+  struct selMC_leg{
+    selMC_leg( int arg_idx );
+    int m_idx;
+    ROOT::VecOps::RVec<edm4hep::MCParticleData> operator() (ROOT::VecOps::RVec<int> list_of_indices,
+							    ROOT::VecOps::RVec<edm4hep::MCParticleData> in) ;
+  };
+
+  ROOT::VecOps::RVec<float> get_both_scalars(ROOT::VecOps::RVec<float> scalar1_value, ROOT::VecOps::RVec<float> scalar2_value);
 
   ROOT::VecOps::RVec<edm4hep::TrackState> get_pseudotrack(ROOT::VecOps::RVec<VertexingUtils::FCCAnalysesVertex> vertex,
 							  ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> recop);
