08caeee44e70eafa38e87ae62d65259fd2749f7e
axgallen <axgallen@lxplus.cern.ch>
2024-12-06 11:25:09 +0100

    Simplified script

--- examples/FCCee/bsm/LLPs/ExoticHiggsDecays/frac_events.py
+++ examples/FCCee/bsm/LLPs/ExoticHiggsDecays/frac_events.py
@@ -6,28 +6,33 @@ samples = ["/eos/experiment/fcc/ee/analyses_storage/BSM/LLPs/ExoticHiggsDecays/F
            "/eos/experiment/fcc/ee/analyses_storage/BSM/LLPs/ExoticHiggsDecays/FINAL_comb/stage1/exoticHiggs_scalar_ms60GeV_sine-5_240912/chunk_0",
            ]
 
-nbins = 1000
-xmin = 0
-xmax = 2000
-
 for sample in samples:
     file_path = f"{sample}.root"
     f = ROOT.TFile(file_path)
     t = f.Get("events")
 
-    hist = ROOT.TH1F("Reco_seltracks_DVs_Lxyz", "Histogram of Reco_seltracks_DVs_Lxyz", nbins, xmin, xmax)
-
+    events_outside = []
+    events_inside = []
+    total_events = []
     for e in t:
         for value in e.Reco_seltracks_DVs_Lxyz:
-            hist.Fill(value)
+            total_events.append(value)
+            if "sine-7" in sample:
+                if value > 2000:
+                    events_outside.append(value)
 
-    # Get the first bin details
-    first_bin_content = hist.GetBinContent(1)
-    first_bin_low_edge = hist.GetBinLowEdge(1)
-    first_bin_high_edge = hist.GetBinLowEdge(2)
+    if "sine-7" in sample:
+        print(sample[-29:])
+        print("----------------------")
+        print("Events w. r>2m: ",len(events_outside))
+        print("Total events: ", len(total_events))
+        print("Fraction: ", 100 * round(len(events_outside)/len(total_events),3),"%")
+        print("======================")
 
-
-    print(f"Sample: {sample}")
-    print(f"First bin range: [{first_bin_low_edge}, {first_bin_high_edge}) and this bin contains {first_bin_content} events")
-    print(f"Total entries in [0,2000]: {hist.GetEntries()}")
-    print("======================")
+    else:   
+        print(sample[-29:])
+        print("----------------------")
+        print("Events w. r<2mm: ",len(events_inside))
+        print("Total events: ", len(total_events))
+        print("Fraction: ", 100 * round(len(events_inside)/len(total_events),3),"%")
+        print("======================")
